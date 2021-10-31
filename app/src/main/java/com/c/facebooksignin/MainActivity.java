f4a15963ed3bc4c83feaac439fedf4696a914d20
jaypithavaelsner <jaypithavaelsner@gmail.com>
2021-10-31 17:05:36 +0530

    Initial commit

---
+++ app/src/main/java/com/c/facebooksignin/MainActivity.java
@@ -0,0 +1,66 @@
+package com.c.facebooksignin;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView info;
+    ImageView profile;
+    LoginButton login;
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        info=findViewById(R.id.info);
+        profile=findViewById(R.id.profile);
+        login=findViewById(R.id.login);
+
+       callbackManager = CallbackManager.Factory.create();
+
+        login.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText("User Id:" +loginResult.getAccessToken().getUserId());
+
+                String imageUrl="https://graph.facebook.com/"+loginResult.getAccessToken().getUserId()+"/picture?type=large";
+                Glide.with(MainActivity.this)
+                        .load(imageUrl)
+                        .into(profile);
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
